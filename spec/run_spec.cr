b599954f9be0b65aa574882c2832e08a97eb2b69
Igor Wessel <igor_wessel@hotmail.com>
2023-10-03 06:38:59 -0300

    test: add correct input format to expect json file

--- spec/run_spec.cr
+++ spec/run_spec.cr
@@ -13,10 +13,72 @@ describe Run do
       first_expect.size.should be >= 1
     end
 
-    it "should have correct input format for expect" do
+    it "should use a custom input file" do
+      temp_input = [{"2" => 4}]
+      tempfile = File.tempfile("100", ".json") do |file|
+        file.print temp_input.to_json
+      end
+      
+      ARGV.push("-i")
+      ARGV.push(tempfile.path)
+      result = Run.load_expect_data
+
+      result.should eq temp_input
+
+      ARGV.clear
+      tempfile.delete
     end
 
-    it "should have correct output format for expect" do
+    it "should have correct input format when is multiple values" do
+      temp_input = [{"1000, 10, 1": 1100}]
+      tempfile = File.tempfile("1", ".json") do |file|
+        file.print temp_input.to_json
+      end
+      
+      ARGV.push("-i")
+      ARGV.push(tempfile.path)
+      result = Run.load_expect_data.as_a
+      exercise = result[0].as_h
+
+      exercise.each_key do |input|
+        input.should contain ", "
+      end
+
+      ARGV.clear
+      tempfile.delete
     end
+
+    it "should have correct input format when is array" do
+      temp_input = [{"['Test']": "Test curtiu isso"}]
+      tempfile = File.tempfile("1", ".json") do |file|
+        file.print temp_input.to_json
+      end
+      
+      ARGV.push("-i")
+      ARGV.push(tempfile.path)
+      result = Run.load_expect_data.as_a
+      exercise = result[0].as_h
+
+      exercise.each_key do |input|
+        input.should start_with "["
+        input.should end_with "]"
+      end
+
+      ARGV.clear
+      tempfile.delete
+    end
+
+    it "should have correct input format when is primitive" do
+    end
+
+    it "should have correct output format when is primitive" do
+    end
+
+    it "should have correct output format when is array" do
+    end
+  end
+
+
+  describe "Run test" do
   end
 end
