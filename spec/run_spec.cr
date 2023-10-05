4ccd44cabaa5daf0b5c750ea38140b3b7d96a86c
Igor Wessel <igor_wessel@hotmail.com>
2023-10-05 08:00:29 -0300

    test: add run test cases for control flow

--- spec/run_spec.cr
+++ spec/run_spec.cr
@@ -1,17 +1,23 @@
 require "./spec_helper"
 
 describe Run do
-  describe "Load expect data" do
-    temp_input = [{"2" => 4}, {"1000, 10, 1" => 1100}, {"['Test']" => "Ok!"}]
+  temp_input = [{"1000, 10, 1" => "1100"}, {"2" => "8", "3" => "28"}, {"['Test']" => "Ok!"}]
 
-    temp_file_input = File.tempfile("100", ".json") do |file|
-        file.print temp_input.to_json
-    end
+  temp_file_input = File.tempfile("expect", ".json") do |file|
+    file.print temp_input.to_json
+  end
 
-    Spec.after_suite {
-      temp_file_input.delete
-    }
+  Spec.after_suite {
+    temp_file_input.delete
+  }
 
+  describe "Load lang data", tags: "fast" do
+    result = Run.load_lang_data
+
+    result.should be_a JSON::Any
+  end
+
+  describe "Load expect data", tags: "fast" do
     it "should have correct format for expect" do
       result = Run.load_expect_data.as_a
       first_expect = result[0]
@@ -27,22 +33,24 @@ describe Run do
       result.should eq JSON.parse(default_expect_file)
     end
 
-    it "should use a custom expect file when pass flag -i" do
-      ARGV.push("-i")
-      ARGV.push(temp_file_input.path)
-      result = Run.load_expect_data
+    {% for flag in ["-i", "--input"] %}
+      it "should use a custom expect file when pass flag {{ flag.id }}" do
+        ARGV.push({{ flag }})
+        ARGV.push(temp_file_input.path)
+        result = Run.load_expect_data
 
-      result.should eq temp_input
+        result.should eq temp_input
 
-      ARGV.clear
-    end
-
+        ARGV.clear
+      end
+    {% end %}
+    
     it "should have correct input format when is multiple values" do
       ARGV.push("-i")
       ARGV.push(temp_file_input.path)
 
       result = Run.load_expect_data.as_a
-      exercise = result[1].as_h
+      exercise = result[0].as_h
 
       exercise.each_key do |input|
         input.should contain ", "
@@ -62,6 +70,37 @@ describe Run do
         input.should start_with "["
         input.should end_with "]"
       end
+
+      ARGV.clear
+    end
+  end
+
+
+  describe "Run test", tags: "slow" do
+    it "should get a error message when not support file extension" do
+      stdout = IO::Memory.new
+      result = Run.run_test(0, "0.error_language", stdout)
+
+      stdout.to_s.should match(/Unsupported file extension/)
+    end
+
+    it "should get a error message when not found exercise" do
+      stdout = IO::Memory.new
+      result = Run.run_test(9999, "9999.cr", stdout)
+
+      stdout.to_s.should match(/Exercise not found: \d+/)
+    end
+
+
+    it "should get correct feedback for success/failed test case" do
+      ARGV.push("-i")
+      ARGV.push(temp_file_input.path)
+
+      stdout = IO::Memory.new
+      result = Run.run_test(1, "samples/1.cr", stdout)
+
+      stdout.to_s.should match(/Successful test for .+ \(/)
+      stdout.to_s.should match(/Test failed for .+ \(/)
 
       ARGV.clear
     end
--- src/run.cr
+++ src/run.cr
@@ -1,14 +1,14 @@
 require "json"
 
 module Run
-  DEFAULT_LANG_JSON_PATH = "./lang/lang.json"
+  DEFAULT_LANG_JSON_PATH   = "./lang/lang.json"
   DEFAULT_EXPECT_JSON_PATH = "./expect/expect.json"
 
   def self.load_lang_data
     JSON.parse(File.read(DEFAULT_LANG_JSON_PATH))
   end
 
-  def self.load_expect_data
+  def self.load_expect_data(stdout : IO = STDOUT)
     expect_json_file = File.read(DEFAULT_EXPECT_JSON_PATH)
 
     if ARGV.includes?("-i") || ARGV.includes?("--input")
@@ -16,21 +16,21 @@ module Run
       if i_index && i_index < ARGV.size - 1
         expect_json_file = File.read(ARGV[i_index + 1])
       else
-        puts "Argument missing. Usage: hackacrow -i INPUT_FILE"
+        stdout.puts "Argument missing. Usage: hackacrow -i INPUT_FILE"
       end
     end
 
     JSON.parse(expect_json_file)
   end
 
-  def self.run_test(exercise_index : Int, file_name : String, run_with_stdin = true)
+  def self.run_test(exercise_index : Int, file_name : String, run_with_stdin = true, stdout : IO = STDOUT)
     lang_data = load_lang_data
     expect_data = load_expect_data
 
     if exercise_index >= 0 && exercise_index < expect_data.size
       exercise = expect_data[exercise_index]
       file_extension = File.extname(file_name).sub(".", "")
-      
+
       if lang_data.as_h.has_key?(file_extension)
         command = "#{lang_data[file_extension]} #{file_name}"
 
@@ -44,16 +44,16 @@ module Run
           output_lines = command_output.split("\n")
           last_line = output_lines.last
           if last_line == value
-            puts "✅ Successful test for #{file_name} (Exercise #{exercise_index} - Input #{key} - Expected #{value} - Got #{last_line})"
+            stdout.puts "✅ Successful test for #{file_name} (Exercise #{exercise_index} - Input #{key} - Expected #{value} - Got #{last_line})"
           else
-            puts "❌ Test failed for #{file_name} (Exercise #{exercise_index} - Input #{key} - Expected #{value} - Got #{last_line})"
+            stdout.puts "❌ Test failed for #{file_name} (Exercise #{exercise_index} - Input #{key} - Expected #{value} - Got #{last_line})"
           end
         end
       else
-        puts "Unsupported file extension: #{file_extension}"
+        stdout.puts "Unsupported file extension: #{file_extension}"
       end
     else
-      puts "Exercise not found: #{exercise_index}"
+      stdout.puts "Exercise not found: #{exercise_index}"
     end
   end
 end
